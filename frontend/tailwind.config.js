14d8b3b1d921c53b66ec9e72a64188d4bccbe889
yhandsome99 <2725147322@qq.com>
2026-03-31 08:53:20 +0800

    Add tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,19 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          900: '#0f172a',
+        },
+        purple: {
+          900: '#2d1b69',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
